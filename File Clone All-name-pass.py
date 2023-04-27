e93c58333f6b931ba9c20780561a3d5ee7705f47
DC Azmir <129629991+dcazmir@users.noreply.github.com>
2023-04-27 14:02:50 +0600

    Update File Clone All-name-pass.py

--- File Clone All-name-pass.py
+++ File Clone All-name-pass.py
@@ -437,7 +437,7 @@ class Main:
 		if Ali in ["2", "02"]:
 			Public()
 		if Ali in ["3", "03"]:
-			os.system("FILE.py")
+			os.system("python FILE.py")
 		if Ali in ["4", "04"]:
 			self.old()
 		if Ali in ["5", "05"]:
